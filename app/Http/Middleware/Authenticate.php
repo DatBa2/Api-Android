b608ef7e15c967054fa741d58445cdf23815d532
datnb7 <DatNB7@fpt.com.vn>
2022-02-15 16:43:16 +0700

    Require JWT auth

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,10 +2,29 @@
 
 namespace App\Http\Middleware;
 
+use Closure;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use \Illuminate\Contracts\Auth\Factory as Auth;
 
 class Authenticate extends Middleware
 {
+    /**
+     * The authentication guard factory instance
+     *
+     * @var \Illuminate\Contracts\Auth\Factory
+     */
+    protected $auth;
+
+    /**
+     * Authenticate constructor.
+     * @param \Illuminate\Contracts\Auth\Factory $auth
+     * @return void
+     */
+    public function __construct(Auth $auth)
+    {
+        $this->auth = $auth;
+    }
+
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      *
@@ -17,5 +36,20 @@ class Authenticate extends Middleware
         if (! $request->expectsJson()) {
             return route('login');
         }
+    }
+
+    /**
+     * @param \Illuminate\Http\Request $request
+     * @param Closure $next
+     * @param string|null $guard
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\Response|mixed
+     */
+    public function handle($request, Closure $next, ...$guard)
+    {
+        if ($this->auth->guard($guard)->guest()) {
+            return response('Unauthorized', 401);
+        }
+
+        return $next($request);
     }
 }
